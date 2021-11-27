415eec4a2c36a12d78e17fe16ed8cf17a9e951f5
Ham9911 <mhammad8633@gmail.com>
2021-11-27 10:49:35 +0500

    a

--- teamreporter/src/App.js
+++ teamreporter/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import 'antd/dist/antd.css';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom"; 
+import Home from './pages/Home';
+import Users from './pages/Users';
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users" element={<Users />} />
+      </Routes>
   );
 }
 
